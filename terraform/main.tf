61664188ccc0e7bc2f50bc98058826978229bc25
Junmin Ahn <junmin@button.is>
2021-06-14 11:52:54 -0700

    chore: destroy all resource created for testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,10 @@ locals {
   prod_user_pool_name = "prod-user-pool"
 }
 
-module "teams" {
-  source = "./teams"
+# module "teams" {
+#   source = "./teams"
 
-  dev_user_pool_id  = aws_cognito_user_pool.dev.id
-  test_user_pool_id = aws_cognito_user_pool.test.id
-  prod_user_pool_id = aws_cognito_user_pool.prod.id
-}
+#   dev_user_pool_id  = aws_cognito_user_pool.dev.id
+#   test_user_pool_id = aws_cognito_user_pool.test.id
+#   prod_user_pool_id = aws_cognito_user_pool.prod.id
+# }
